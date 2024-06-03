91eeddc4dd293e9b9887495686d076fcb5df12c2
AlexeiTim <86958614+AlexeiTim@users.noreply.github.com>
2024-06-03 22:23:44 +0300

    добавил ввод поля поиска

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
-import { ReactNode, Component } from "react"
+import { ReactNode, Component, ChangeEvent } from "react"
 import "./App.css"
 
-export class App extends Component {
+export class App extends Component<object, { search: string }> {
+  constructor(props: object) {
+    super(props)
+    this.state = {
+      search: "",
+    }
+
+    this.handleChangeSearch = this.handleChangeSearch.bind(this)
+  }
+
+  handleChangeSearch(e: ChangeEvent<HTMLInputElement>) {
+    this.setState({ search: e.target.value })
+  }
+
   render(): ReactNode {
     return (
       <div>
-        <h1>Hello world</h1>
+        <section>
+          <div>
+            <input
+              type="text"
+              value={this.state.search}
+              onInput={this.handleChangeSearch}
+            />
+            <button>Поиск</button>
+          </div>
+        </section>
+        <section>
+          <div>Список</div>
+        </section>
       </div>
     )
   }
