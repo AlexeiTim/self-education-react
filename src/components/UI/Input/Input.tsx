c9ee175ec7d09d025f851a71c3e2e98b90634993
AlexeiTim <86958614+AlexeiTim@users.noreply.github.com>
2024-06-05 23:50:47 +0300

    линтер

--- src/components/UI/Input/Input.tsx
+++ src/components/UI/Input/Input.tsx
@@ -1,10 +1,7 @@
 import React from "react"
 import styles from "./Input.module.css"
 
-type Props = React.DetailedHTMLProps<
-  React.InputHTMLAttributes<HTMLInputElement>,
-  HTMLInputElement
->
+type Props = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 
 export class Input extends React.Component<Props> {
   render(): React.ReactNode {
